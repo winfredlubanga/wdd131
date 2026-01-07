cfc578e24f07aae49d2f551c935b6e149b9e544d
winfredlubanga <wlubanga1@byupathway.edu>
2026-01-07 17:37:02 +0300

    add this to mainadd all to git now

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Display current year
+document.getElementById("currentyear").textContent =
+    new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
